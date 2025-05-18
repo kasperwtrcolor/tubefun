5a98ffdd4263def94fe2ebea79a47f3c058e0a19
Kasper <106410740+kasperwtrcolor@users.noreply.github.com>
2025-05-18 23:57:56 +0200

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.post("/api/proxy/ipfs", upload.none(), async (req, res) => {
       },
       {
         headers: {
-          Authorization: `Bearer ${eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiJjNDFmYTczZi04Mjc0LTRkMTEtYTk3NC0yZWU5N2ZiZGFmNGQiLCJlbWFpbCI6InJhc21hc3N0dWRpb0BnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwicGluX3BvbGljeSI6eyJyZWdpb25zIjpbeyJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MSwiaWQiOiJGUkExIn0seyJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MSwiaWQiOiJOWUMxIn1dLCJ2ZXJzaW9uIjoxfSwibWZhX2VuYWJsZWQiOmZhbHNlLCJzdGF0dXMiOiJBQ1RJVkUifSwiYXV0aGVudGljYXRpb25UeXBlIjoic2NvcGVkS2V5Iiwic2NvcGVkS2V5S2V5IjoiNDI1MTgxNmUzZTJlZjlhZjNhYzciLCJzY29wZWRLZXlTZWNyZXQiOiIwOGFjZDJhZjI3YjIxZjViOGY4ODI3Mjk4MjIyOGQ5NmM3ZTg3MWIxMjY3NDM5NjZlNTYzMzgzYzEzNTEwYmYyIiwiZXhwIjoxNzc5MTQxMjQ2fQ.8QV4aviEJSGgM0Fukyb0Tsd-opltTJw6nOzs84dExS8}`,
+          Authorization: `Bearer ${PINATA_JWT}`,
           "Content-Type": "application/json"
         }
       }
